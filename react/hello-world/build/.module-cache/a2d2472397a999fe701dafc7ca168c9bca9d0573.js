d61987fb6ee3a81548f27f96b2bd7326a50f6a6f
Martin Rodriguez <Martin.Rodriguez@wnco.com>
2014-09-18 11:04:05 -0500

    Martin - Merged from bitbucket - React tutorial - Socket.io - Both work in progress - Finished chat-example socket.io

---
+++ react/hello-world/build/.module-cache/a2d2472397a999fe701dafc7ca168c9bca9d0573.js
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+React.renderComponent(
+	React.DOM.h1(null, "Hello World"),
+	document.getElementById('example')
+);
---
+++ react/hello-world/src/helloworld.js
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+React.renderComponent(
+	<h1>Hello World</h1>,
+	document.getElementById('example')
+);
