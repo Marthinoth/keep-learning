81903942e67849de87c09cd714587465cc8da3aa
Martin Rodriguez <marthinoth@gmail.com>
2014-09-26 11:08:10 -0500

    Martin - added build folder to gitignore - hello-react - added comment component

--- react/hello-react/public/src/components/comment-list.jsx
+++ react/hello-react/public/src/components/comment-list.jsx
@@ -1,12 +1,14 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var Comment = require('./comment.jsx');
 
 var CommentList = React.createClass({
 	render: function() {
 		return (
 			<div className="commentList">
-				comment list
+				<Comment author="Pete Hunt">This is one comment</Comment>
+				<Comment author="Jordan Walke">This is another comment</Comment>
 			</div>
 		);
 	}
