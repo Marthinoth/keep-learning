0f53a45075cbba1c7d0a76dd1f3580c010a57011
Marthinoth <marthinoth@gmail.com>
2014-09-27 22:37:32 -0300

    Martin - Added initial breakout tutorial example using Phaser

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ This is my own repository to upload some practices done while learning new stuff
 * **node-school-io:** Practices from [NodeSchool] - Learn node.js with interactive lessons
   * **learn-your-code:** Practices for the basics of node: asynchronous i/o, http
 * **phaser:** [Phaser] is a fast, free and fun open source game framework for making desktop and mobile browser HTML5 games
-  * **flappy-bird-phaser:**  practice taken from [How to make a Flappy Bird in HTML5 - Part 1]
+  * **flappy-bird-phaser:** practice taken from [How to make a Flappy Bird in HTML5 - Part 1]
+  * **simple-breakout:**  how to use Phaser to build a really simple retro breakout clone [dailyjs-phase-tutorial]
 * **react:** practice related to [React], a javascript library for building user interfaces
   * **hello-react:** starter tutorial from [react-tutorial]
   * **hello-world:** very basic tutorial taken from [getting-started]
@@ -20,6 +21,7 @@ This is my own repository to upload some practices done while learning new stuff
 [NodeSchool]:http://nodeschool.io/
 [Phaser]:http://phaser.io/
 [How to make a Flappy Bird in HTML5 - Part 1]:http://blog.lessmilk.com/how-to-make-flappy-bird-in-html5-1/
+[dailyjs-phase-tutorial]:http://dailyjs.com/2014/09/16/phaser-tutorial/
 [React]:http://facebook.github.io/react/
 [getting-started]:http://facebook.github.io/react/docs/getting-started.html
 [react-tutorial]:http://facebook.github.io/react/docs/tutorial.html
---
+++ phaser/simple-breakout/main.js
@@ -0,0 +1,54 @@
+var main = {
+  preload: function() {
+    game.load.image('paddle', 'assets/paddle.png');
+    game.load.image('bridge', 'assets/brick.png');
+    game.load.image('ball', 'assets/ball.png');
+  },
+
+  create: function() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.cursor = game.input.keyboard.createCursorKeys();
+
+    this.paddle = game.add.sprite(200, 400, 'paddle');
+    game.physics.arcade.enable(this.paddle);
+    this.paddle.body.immovable = true;
+
+    this.bricks = game.add.group();
+    this.bricks.enableBody = true;
+    for(var i = 0; i < 5; i++) {
+      for(var j = 0; j < 5; j++) {
+        game.add.sprite(55+i*60, 55+j*35,'bridge', 0, this.bricks);
+      }
+    }
+    this.bricks.setAll('body.immovale', true);
+
+    this.ball = game.add.sprite(200, 300, 'ball');
+    game.physics.arcade.enable(this.ball);
+    this.ball.body.velocity.x = 200;
+    this.ball.body.velocity.y = 200;
+    this.ball.body.collideWorldBounds = true;
+    this.ball.body.bounce.x = 1;
+    this.ball.body.bounce.y = 1;
+  },
+
+  update: function() {
+    if(this.cursor.right.isDown) {
+      this.paddle.body.velocity.x = 350;
+    } else if(this.cursor.left.isDown) {
+      this.paddle.body.velocity.x = -350;
+    } else {
+      this.paddle.body.velocity.x = 0;
+    }
+    game.physics.arcade.collide(this.paddle, this.ball);
+    game.physics.arcade.collide(this.ball, this.bricks, this.hit, null, this);
+  },
+
+  hit: function(ball, brick) {
+    brick.kill();
+  }
+};
+
+var game = new Phaser.Game(400, 450, Phaser.AUTO, 'gameDiv');
+game.state.add('main', main);
+game.state.start('main');
